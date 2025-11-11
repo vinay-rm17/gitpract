7d476a20a80a76b23b1d4f2ab7e56931af98bfc1
vinay6363755871 <rmvinayvinay@gmail.com>
2025-11-11 14:53:41 +0530

    changes made in main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def greet():
+def greet(Name=None):
+    if Name:
+        return f"Hello {Name},from main"
     return "Hello from Main!"
 
 if __name__=="__main__":
-    print(greet())
+    print(greet("Vinay"))
